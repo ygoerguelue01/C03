70d967111bf8914243ec69e8652766cc83db33ce
dmodliba <dmodliba@student.tgm.ac.at>
2017-03-13 10:23:50 +0100

    V01

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		
+	}
+	
+	public static void dmodliba(){
+		System.out.println("Schöne Grüße von Daniel Modliba!");
+	}
+	
+}
